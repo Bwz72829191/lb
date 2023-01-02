247a1446b9dc0462427e98eb75c2417e9b93530f
Bwxteam <111744629+Bwz72829191@users.noreply.github.com>
2023-01-02 18:24:42 +0300

    Create dos.php

---
+++ dos.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Home page</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<button class="menu-btn"></button>
+
+<div class="overlay">
+  <div class="text">
+    <ul>
+      <li><a href="https://ddos.spammer.gq/">Home</a></li>
+      <li><a href="https://ddos.spammer.gq/attack">Attack panel</a></li>
+      <li><a href="https://ddos.spammer.gq/info">Information</a></li>
+    </ul>
+  </div>
+</div>
+<!-- partial -->
+  <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="./script.js"></script>
+
+</body>
+</html>
